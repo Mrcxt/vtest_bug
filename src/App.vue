8826b4fb5266aef6f4535f5438395b0e5663752b
mrcxt <847428891@qq.com>
2019-11-27 14:24:39 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
+<!--
+ * @LastEditTime: 2019-11-27 11:48:12
+ * @Description: 
+ * @Date: 2019-11-27 11:44:55
+ * @Author: @虾哔哔
+ -->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Test1 />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	import Test1 from "./components/test1";
+	export default {
+		name: "app",
+		components: {
+			Test1
+		}
+	};
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+	}
 </style>
---
+++ src/components/test1.vue
@@ -0,0 +1,34 @@
+<!--
+ * @LastEditTime: 2019-11-27 14:15:50
+ * @Description: 
+ * @Date: 2019-11-27 11:46:51
+ * @Author: @虾哔哔
+ -->
+<template>
+  <div>
+    {{ test() }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tag: null
+    };
+  },
+  methods: {
+    test() {
+      console.log("object");
+
+      /*----------  添加上下面这段代码之后，就会被执行2次  ----------*/
+      if (!this.tag) {
+        this.tag = 1; //关键点，更改了 this.tag 的状态
+      }
+      /*----------    ----------*/
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
