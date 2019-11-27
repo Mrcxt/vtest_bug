9a30c32271dc18c19b64e967f51c5a2f069cc4e1
mrcxt <847428891@qq.com>
2019-11-27 15:51:25 +0800

    refactor: add test code

--- src/components/test1.vue
+++ src/components/test1.vue
@@ -1,5 +1,5 @@
 <!--
- * @LastEditTime: 2019-11-27 14:15:50
+ * @LastEditTime: 2019-11-27 15:08:09
  * @Description: 
  * @Date: 2019-11-27 11:46:51
  * @Author: @虾哔哔
@@ -13,19 +13,20 @@
 <script>
 export default {
   data() {
-    return {
-      tag: null
-    };
+    return {};
   },
+  tag: null,
   methods: {
     test() {
       console.log("object");
 
       /*----------  添加上下面这段代码之后，就会被执行2次  ----------*/
-      if (!this.tag) {
-        this.tag = 1; //关键点，更改了 this.tag 的状态
-      }
+      //   if (!this.tag) {
+      //     this.tag = 1; //关键点，更改了 this.tag 的状态
+      //   }
       /*----------    ----------*/
+      let aaa = this.tag;
+      this.tag = 1; // 关键点，如果不用if包裹，那么只会执行一次
     }
   }
 };
